8f48743cf111142cdfcd4b6f8f48fdee89fe5409
nahim888 <ahmed.nahim@iisgalvanimi.edu.it>
2024-05-17 10:42:01 +0000

    commit 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  data: any;
+  constructor(private http: HttpClient)
+  {
+    this.http.get("https://5000-nahim888-progettodocker-ybd4d2n9n6r.ws-eu111.gitpod.io/simple_json")
+    .subscribe(
+      (data) => this.data = data
+    )
+  }
 }
--- server/app.py
+++ server/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def ping_server():
     return "Welcome to the world of animals."
 
+@app.route('/simple_json')
+def simple_json():
+    return jsonify('{saluto:ciao}')
+
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
